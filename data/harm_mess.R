3b104667402ebf55dc897fa92ae4a78931e5779b
Milan Valasek <mivalek@gmail.com>
2020-02-07 19:10:08 +0000

    harm data mess-up code

--- data/harm_mess.R
+++ data/harm_mess.R
@@ -2,21 +2,18 @@
 
 library(tidyverse)
 set.seed(1)
-df <- read_csv("analysing_data/data/harm2.csv")
+df <- read_csv("analysing_data/data/harm.csv")
+df <- df %>% select(-c(Age, Country, Ethnicity)) %>%
+  rename(Age = AgeNew)
 
-# get rid of unneeded columns
-df <- df[, -c(1:4, 6:11)]
 
-# discard withdrawn data
-df <- filter(df, is.na(Debrief_1))
-df <- df[df$Age != "17" & !is.na(df$Age), 1:13 ]
+df$Gender[sample(nrow(df), 14)] <- NA
 
-df <- rbind(df, df[sample(nrow(df), 17), ])
 
-df$Gender[301:303] <- NA
-df$Age[304:310] <- c(sample(150:540, 7))
-df[311:317, 3:13] <- NA
-df <- df[sample(nrow(df)), ]
-names(df)[1] <- "Duration"
-write_csv(df, "analysing_data/gen_sex_q.csv")
+df$Age[sample(nrow(df), 5)] <- c(199, 223, 6, 118, "1y")
+
+df[ , c("Experience", "Agency", "Consciousness", "Empathy")] <- lapply(
+  df[ , c("Experience", "Agency", "Consciousness", "Empathy")], round)
+
+write_csv(df, "analysing_data/data/harm_data.csv")
 
